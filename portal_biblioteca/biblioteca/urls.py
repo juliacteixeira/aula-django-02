d8a2b20c2df84db9aeec8e611825559082c11891
Julia Teixeira <juuhc.teixeira@gmail.com>
2023-09-20 23:30:02 +0000

    páginas principal, tccs, livros e base

---
+++ portal_biblioteca/biblioteca/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.principal, name='principal'),
+    path('livros', views.livros, name='livros'),
+    path('tccs', views.tccs, name='tccs'),
+    path('tccs/detalhes/<int:id>', views.tcc_detalhes, name='tcc_detalhes'),
+]
